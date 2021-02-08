f6c1932fa8b68534986041dc106843bd67de38bb
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-08 14:29:30 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       ressources :airlines, params :slug
-      ressources :reviews, only [:create, :destroy]
+      ressources :reviews, only: [:create, :destroy]
     end
   end
 
