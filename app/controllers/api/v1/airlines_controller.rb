9fcdbcc1cd57df4328084dcc67006cc91bc7b6d5
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-08 14:58:06 -0500

    added methods for reviews

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -4,12 +4,12 @@ module Api
             def index
                 airlines = Airline.All
 
-                render json: AirlineSerializer.new(airlines).serialized_json
+                render json: AirlineSerializer.new(airlines, options).serialized_json
             end
 
             def show
                 airline = Airline.find_by(slug: params[:slug])
-                render json: AirlineSerializer.new(airlines).serialized_json
+                render json: AirlineSerializer.new(airlines, options).serialized_json
             end
 
             def create
@@ -26,7 +26,7 @@ module Api
                 airline = Airline.find_by(slug: params[:slug])
 
                 if airline.update(airline_params)
-                    render json: AirlineSerializer.new(airlines).serialized_json
+                    render json: AirlineSerializer.new(airlines, options).serialized_json
                 else
                      render json: {error: airline.errors.messages}, status: 422
                 end
@@ -46,6 +46,10 @@ module Api
 
             def airline_params
                 params.require(:airline).permit(:name, :image_url)
+            end
+
+            def options
+                @options ||= { include: %i[reviews] }
             end
         end
     end
